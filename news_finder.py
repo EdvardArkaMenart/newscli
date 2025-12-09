e7289098c91ded1b5c9a156da8d37403578387b6
edvard arka menart <edvard.arka.menart@gmail.com>
2025-12-09 14:56:49 +0100

    experiment

--- news_finder.py
+++ news_finder.py
@@ -1,8 +1,13 @@
 import urllib3
-import beautifulsoup4
+from bs4 import BeautifulSoup
 
 http = urllib3.PoolManager()
 resp = urllib3.request("get", "https://www.vg.no")
 data_bytes = resp.data
 data_string = data_bytes.decode("utf-8")
-print(data_string)
+soup = BeautifulSoup(data_string, 'html.parser')
+
+print(soup.head)
+#print(soup.get_text())
+#for link in soup.find_all('a'):
+    #print(link.get('href'))
