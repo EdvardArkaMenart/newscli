a82ae8286f1d35d0e768dcce8ef2afb13543386a
edvard arka menart <edvard.arka.menart@gmail.com>
2025-12-11 16:19:24 +0100

    testing

--- news_finder.py
+++ news_finder.py
@@ -6,8 +6,10 @@ resp = urllib3.request("get", "https://www.vg.no")
 data_bytes = resp.data
 data_string = data_bytes.decode("utf-8")
 soup = BeautifulSoup(data_string, 'html.parser')
-
-print(soup.head)
+head_tag = soup.head
+print(soup.find_all('a'))
+#print(len(list(soup.descendants)))
+#print(head_tag.contents)
 #print(soup.get_text())
 #for link in soup.find_all('a'):
     #print(link.get('href'))
