182749bb07857bf3ad5fbf868c50db97cd16f7de
edvard arka menart <edvard.arka.menart@gmail.com>
2025-12-14 15:41:53 +0100

    test

--- news_finder.py
+++ news_finder.py
@@ -1,3 +1,6 @@
+# finn resten av overskriften
+# sjekke om artikel er synelig
+
 import urllib3
 from bs4 import BeautifulSoup
 
@@ -9,15 +12,22 @@ soup = BeautifulSoup(data_string, 'html.parser')
 head_tag = soup.head
 results = soup.find(id="hovedlopet")
 
-job_cards = results.find_all("div", class_="articles")
+job_cards = results.find_all("div", "article-container")
+#print(job_cards)
 for job_card in job_cards:
-    title_element = job_card.find("h2", class_="title")
-    company_element = job_card.find("h3", class_="company")
+    title_element = job_card.find("h2", class_="headline")
+    company_element = job_card.find_all("a", href=True)
     location_element = job_card.find("p", class_="location")
-    print(title_element)
-    print(company_element)
-    print(location_element)
-    print()
+    #print(type(company_element))
+    print("*************************")
+    print(title_element.text.strip())
+    for i in company_element:
+        
+        print(i["href"])
+    
+    #print(type(company_element))
+    #print(location_element)
+    #print()
 #print(soup.find_all('a'))
 #print(len(list(soup.descendants)))
 #print(head_tag.contents)
