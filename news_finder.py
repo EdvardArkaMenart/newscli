3f9578013899b65794d808cb20c8ae2239d6bbb2
edvard arka menart <edvard.arka.menart@gmail.com>
2025-12-14 13:26:14 +0100

    d

--- news_finder.py
+++ news_finder.py
@@ -7,9 +7,20 @@ data_bytes = resp.data
 data_string = data_bytes.decode("utf-8")
 soup = BeautifulSoup(data_string, 'html.parser')
 head_tag = soup.head
-print(soup.find_all('a'))
+results = soup.find(id="hovedlopet")
+
+job_cards = results.find_all("div", class_="articles")
+for job_card in job_cards:
+    title_element = job_card.find("h2", class_="title")
+    company_element = job_card.find("h3", class_="company")
+    location_element = job_card.find("p", class_="location")
+    print(title_element)
+    print(company_element)
+    print(location_element)
+    print()
+#print(soup.find_all('a'))
 #print(len(list(soup.descendants)))
 #print(head_tag.contents)
-#print(soup.get_text())
+#print(soup.text)
 #for link in soup.find_all('a'):
     #print(link.get('href'))
